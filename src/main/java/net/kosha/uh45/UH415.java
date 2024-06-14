2c95cca538563f4fb9c3f1f338ebe938c25a03d1
Kosha41x2 <89689438+Kosha41x2@users.noreply.github.com>
2024-06-14 19:57:52 +0200

    Test

--- src/main/java/net/kosha/uh45/UH415.java
+++ src/main/java/net/kosha/uh45/UH415.java
@@ -2,6 +2,7 @@ package net.kosha.uh45;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.kosha.uh45.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,7 @@ public class UH415 implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
 	}
 }
