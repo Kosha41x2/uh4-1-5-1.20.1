92f036d622a919083ac55ae5f4a51a1df76d9d73
Kosha41x2 <89689438+Kosha41x2@users.noreply.github.com>
2025-05-11 16:33:44 +0200

    Signs added. Corruption block to parch.

---
+++ src/main/java/net/kosha/uh45/entity/custom/ModBoats.java
@@ -0,0 +1,8 @@
+package net.kosha.uh45.entity.custom;
+
+import net.kosha.uh45.UH415;
+import net.minecraft.util.Identifier;
+
+public class ModBoats {
+    public static final Identifier DEAD_BOAT_ID = new Identifier(UH415.MOD_ID, "dead_boat");
+}
